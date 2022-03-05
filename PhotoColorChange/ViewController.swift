1eea6a48d0f310555e0831d37f1cddd9f51a7986
Vital <slyvitax@mail.ru>
2022-03-05 10:49:27 +0300

    Before change code

--- PhotoColorChange/ViewController.swift
+++ PhotoColorChange/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewController: UIView!
+   
+    @IBOutlet var redInfoText: UITextField!
+    @IBOutlet var greenInfoText: UITextField!
+    @IBOutlet var blueInfoText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redSliderChange() {
+    }
+    
+    @IBAction func greenSliderChange() {
+    }
+    
+    @IBAction func blueSliderChange() {
+    }
+    
 }
 
