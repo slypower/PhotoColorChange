c7ac83459539c1cc94d3733abcf20f18180cdad1
Vital <slyvitax@mail.ru>
2022-03-05 11:15:31 +0300

    redrawn

--- PhotoColorChange/ViewController.swift
+++ PhotoColorChange/ViewController.swift
@@ -10,23 +10,18 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var viewController: UIView!
    
-    @IBOutlet var redInfoText: UITextField!
-    @IBOutlet var greenInfoText: UITextField!
-    @IBOutlet var blueInfoText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewController.layer.cornerRadius = 10
     }
 
-    @IBAction func redSliderChange() {
-    }
     
-    @IBAction func greenSliderChange() {
-    }
     
-    @IBAction func blueSliderChange() {
+    /*
+    private func showView {
+        viewController.backgroundColor
     }
-    
+    */
 }
 
